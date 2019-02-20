ff2ee1a078f09b3b2a7e7974ee40fad1318354a0
Stefan Pflueger <spfluege@gmail.com>
2019-02-20 15:28:10 +0800

    Added normalization decorators for intensity and amplitude - Added a NormalizationAmplitudeDecorator and an   NormalizationIntensityDecorator class, which can be used for normalization. However their use is discouraged as the fit performance decreases dramatically. - Small rework of integration functionality: Created an   IntegrationStrategy interface, and a concrete Monte Carlo   integration strategy. - Introduced multi threading in the MC integration strategy. - Removed Efficiency member from the Event and DataPoint classes.

---
+++ Physics/NormalizationAmplitudeDecorator.cpp
@@ -0,0 +1,88 @@
+// Copyright (c) 2013, 2017 The ComPWA Team.
+// This file is part of the ComPWA framework, check
+// https://github.com/ComPWA/ComPWA/license.txt for details.
+
+#include <cmath>
+
+#include "Core/Event.hpp"
+#include "Data/DataTransformation.hpp"
+#include "NormalizationAmplitudeDecorator.hpp"
+#include "Physics/CoherentIntensity.hpp"
+#include "Tools/Integration.hpp"
+
+namespace ComPWA {
+namespace Physics {
+
+NormalizationAmplitudeDecorator::NormalizationAmplitudeDecorator(
+    const std::string &name, std::shared_ptr<NamedAmplitude> amplitude,
+    std::shared_ptr<ComPWA::Tools::IntegrationStrategy> integrator)
+    : NamedAmplitude(name), UnnormalizedAmplitude(amplitude),
+      NormedAmplitude(std::make_shared<CoherentIntensity>(
+          amplitude->getName(),
+          std::vector<std::shared_ptr<Amplitude>>{amplitude})),
+      Integrator(integrator) {
+  Normalization = std::sqrt(1.0 / Integrator->integrate(NormedAmplitude));
+  ParameterList TempParList;
+  UnnormalizedAmplitude->addUniqueParametersTo(TempParList);
+  PreviousParameterList.DeepCopy(TempParList);
+}
+
+std::complex<double> NormalizationAmplitudeDecorator::evaluate(
+    const ComPWA::DataPoint &point) const {
+
+  return Normalization * UnnormalizedAmplitude->evaluate(point);
+}
+
+void NormalizationAmplitudeDecorator::updateParametersFrom(
+    const ParameterList &list) {
+  // it is important to call the update on the unnormalized intensity first
+  UnnormalizedAmplitude->updateParametersFrom(list);
+
+  ParameterList TempParList;
+  UnnormalizedAmplitude->addUniqueParametersTo(TempParList);
+  for (unsigned int i = 0; i < TempParList.doubleParameters().size(); ++i) {
+    LOG(DEBUG) << TempParList.doubleParameters().size() << "  "
+               << PreviousParameterList.doubleParameters().size();
+    if (TempParList.doubleParameter(i) !=
+        PreviousParameterList.doubleParameter(i)) {
+      Normalization = std::sqrt(1.0 / Integrator->integrate(NormedAmplitude));
+      PreviousParameterList.DeepCopy(TempParList);
+      break;
+    }
+  }
+}
+
+void NormalizationAmplitudeDecorator::addUniqueParametersTo(
+    ParameterList &list) {
+  UnnormalizedAmplitude->addUniqueParametersTo(list);
+}
+
+std::shared_ptr<FunctionTree>
+NormalizationAmplitudeDecorator::createFunctionTree(
+    const ParameterList &DataSample, const std::string &suffix) const {
+
+  auto NodeName = "NormalizedAmplitude(" + getName() + ")" + suffix;
+  size_t n = DataSample.mDoubleValue(0)->values().size();
+
+  auto tr = std::make_shared<FunctionTree>(
+      NodeName, MComplex("", n), std::make_shared<MultAll>(ParType::MCOMPLEX));
+
+  tr->createNode("sqrt(Normalization)", ComPWA::ValueFactory(ParType::DOUBLE),
+                 std::make_shared<ComPWA::SquareRoot>(ParType::DOUBLE),
+                 NodeName);
+  auto normtree = Integrator->createFunctionTree(NormedAmplitude, "_ampnorm");
+  tr->insertTree(normtree, "sqrt(Normalization)");
+  auto intenstree =
+      UnnormalizedAmplitude->createFunctionTree(DataSample, suffix);
+  tr->insertTree(intenstree, NodeName);
+
+  return tr;
+}
+
+std::shared_ptr<const Amplitude>
+NormalizationAmplitudeDecorator::getUnnormalizedAmplitude() const {
+  return UnnormalizedAmplitude;
+}
+
+} // namespace Physics
+} // namespace ComPWA
---
+++ Physics/NormalizationIntensityDecorator.cpp
@@ -0,0 +1,78 @@
+// Copyright (c) 2013, 2017 The ComPWA Team.
+// This file is part of the ComPWA framework, check
+// https://github.com/ComPWA/ComPWA/license.txt for details.
+
+#include "NormalizationIntensityDecorator.hpp"
+#include "Core/Event.hpp"
+#include "Data/DataTransformation.hpp"
+#include "Tools/Integration.hpp"
+
+namespace ComPWA {
+namespace Physics {
+
+NormalizationIntensityDecorator::NormalizationIntensityDecorator(
+    const std::string &name, std::shared_ptr<ComPWA::Intensity> intensity,
+    std::shared_ptr<ComPWA::Tools::IntegrationStrategy> integrator)
+    : Name(name), UnnormalizedIntensity(intensity), Integrator(integrator) {
+
+  Normalization = 1.0 / Integrator->integrate(UnnormalizedIntensity);
+  ParameterList TempParList;
+  UnnormalizedIntensity->addUniqueParametersTo(TempParList);
+  PreviousParameterList.DeepCopy(TempParList);
+}
+
+double NormalizationIntensityDecorator::evaluate(
+    const ComPWA::DataPoint &point) const {
+
+  return Normalization * UnnormalizedIntensity->evaluate(point);
+}
+
+void NormalizationIntensityDecorator::updateParametersFrom(
+    const ParameterList &list) {
+  // it is important to call the update on the unnormalized intensity first
+  UnnormalizedIntensity->updateParametersFrom(list);
+
+  ParameterList TempParList;
+  UnnormalizedIntensity->addUniqueParametersTo(TempParList);
+  for (unsigned int i = 0; i < TempParList.doubleParameters().size(); ++i) {
+    if (TempParList.doubleParameter(i) !=
+        PreviousParameterList.doubleParameter(i)) {
+      Normalization = 1.0 / Integrator->integrate(UnnormalizedIntensity);
+      PreviousParameterList.DeepCopy(TempParList);
+      break;
+    }
+  }
+}
+
+void NormalizationIntensityDecorator::addUniqueParametersTo(
+    ParameterList &list) {
+  UnnormalizedIntensity->addUniqueParametersTo(list);
+}
+
+std::shared_ptr<FunctionTree>
+NormalizationIntensityDecorator::createFunctionTree(
+    const ParameterList &DataSample, const std::string &suffix) const {
+
+  auto NodeName = "NormalizedIntensity(" + Name + ")" + suffix;
+  size_t n = DataSample.mDoubleValue(0)->values().size();
+
+  auto tr = std::make_shared<FunctionTree>(
+      NodeName, MDouble("", n), std::make_shared<MultAll>(ParType::MDOUBLE));
+
+  auto normtree =
+      Integrator->createFunctionTree(UnnormalizedIntensity, "_norm");
+  tr->insertTree(normtree, NodeName);
+  auto intenstree =
+      UnnormalizedIntensity->createFunctionTree(DataSample, suffix);
+  tr->insertTree(intenstree, NodeName);
+
+  return tr;
+}
+
+std::shared_ptr<const ComPWA::Intensity>
+NormalizationIntensityDecorator::getUnnormalizedIntensity() const {
+  return UnnormalizedIntensity;
+}
+
+} // namespace Physics
+} // namespace ComPWA
